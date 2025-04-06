ac7bf17b0e8f57709642a19ac34d30645a6e0cea
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-04-07 01:04:56 +0530

    Rust libp2p node implementations

---
+++ rust-libp2p/src/main.rs
@@ -0,0 +1,23 @@
+use std::error::Error;
+
+use libp2p::{noise, tcp, yamux};
+use tracing_subscriber::EnvFilter;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .try_init();
+
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+        .with_tokio()
+        .with_tcp(
+            tcp::Config::default(),
+            noise::Config::new,
+            yamux::Config::default,
+        )?;
+
+    Ok(())
+}
